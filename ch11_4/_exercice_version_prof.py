2b2d947995b9a5f0c6ba894a4a7318eb2e172775
Andjela Dimitrijevic <46799444+Andjelaaaa@users.noreply.github.com>
2024-11-10 20:35:33 -0500

    Sauvegarde du fichier dans un dossier

--- ch11_4/_exercice_version_prof.py
+++ ch11_4/_exercice_version_prof.py
@@ -10,12 +10,12 @@ import os
 from _music_library_version_prof import Playlist, Song
 
 
-def manage_playlist(sort_key: str, file_path: str):
+def manage_playlist(sort_key: str, file_name: str):
     """
     Crée une playlist, trie les chansons et exporte la liste triée au format JSON.
 
     :param sort_key: Clé selon laquelle trier les chansons (title, artist, duration, genre)
-    :param file_path: Chemin pour exporter le fichier JSON
+    :param file_name: Nom du fichier JSON pour exporter la playlist
     """
     # Initialisation des chansons
     print("Initialisation des chansons...\n")
@@ -41,14 +41,22 @@ def manage_playlist(sort_key: str, file_path: str):
     playlist.sort_songs(sort_key)
     playlist.display()
 
+    # Création du répertoire `libraries` s'il n'existe pas
+    output_dir = "libraries"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        print(f"Répertoire '{output_dir}' créé.")
+
     # Exportation en JSON
+    file_path = os.path.join(output_dir, file_name)
     print(f"\nExportation de la playlist triée vers le fichier : {file_path} ...")
     playlist.export_to_json(file_path)
     print(f"Exportation réussie au chemin : {os.path.abspath(file_path)}")
 
 
 def main():
-    manage_playlist(sort_key="duration", file_path="sorted_playlist.json")
+    manage_playlist(sort_key="duration", file_name="sorted_playlist.json")
+
 
 if __name__ == "__main__":
-	main()
+    main()
